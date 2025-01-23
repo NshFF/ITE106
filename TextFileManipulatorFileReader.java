ee856f8d7423b7952bb397841790c0ee9f0bb79e
Nash Fulgencio <fulgencionash@gmail.com>
2025-01-23 12:28:10 +0800

    Add files via upload

---
+++ TextFileManipulatorFileReader.java
@@ -0,0 +1,13 @@
+import java.io.File;
+public class TextFileManipulatorFileReader
+{
+	public static void main(String[] args) throws Exception
+	{
+
+	File file = new File("jancarluzdeleon.txt");
+int i;
+	
+	while ((i - fr.read()) != -1)
+		System.out.println((char)i);
+	}
+}
